2f9e8ef177f51800b32444c9038ba9f747cd5141
Macbook <musadiqraees@gmail.com>
2018-01-23 20:09:12 +0100

    uploading code for first time

---
+++ TASK2/SharedManager.swift
@@ -0,0 +1,63 @@
+//
+//  SharedManager.swift
+//  TASK2
+//
+//  Created by Macbook on 1/20/18.
+//  Copyright © 2018 Macbook. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+//////extnsion for Alert
+extension UIViewController {
+    
+    func alert(message: String, title: String = "Error") {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
+//////extenstion for keyboard
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+class SharedManager {
+    static let sharedInstance = SharedManager()
+    private init() {}
+    var locationManager = CLLocationCoordinate2D()
+    
+    func setApiToken(token:String) -> ()
+    {
+     UserDefaults.standard.setValue(token, forKey: "user_auth_token")
+    }
+    
+    func getApiToken() -> (String) {
+        let token =   UserDefaults.standard.value(forKey: "user_auth_token") as! String
+        return token
+    }
+    
+    func setLocation(location: CLLocationCoordinate2D) -> ()
+    {
+        self.locationManager = location
+    }
+    
+    func getLocation() -> (CLLocationCoordinate2D)
+    {
+        return self.locationManager
+    }
+    
+    
+}
+   
